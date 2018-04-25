b04f4fbee0110f634786c1f068ce74ae568fa108
Kriti Rai <kriti.rai@gmail.com>
2018-04-25 04:43:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,4 +26,12 @@ class Student
       DB[:conn].execute("DROP TABLE students")
     end
 
+    def save
+      sql = <<-SQL
+        INSERT INTO students (name,grade)
+        VALUES (?,?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+      end
+
 end
